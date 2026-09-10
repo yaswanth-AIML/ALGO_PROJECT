aa196d3959aad3c4d6206cbc6605ae3ee6087439
Yaswanth <yaswanthvanacharla@gmail.com>
2026-09-10 21:44:55 +0530

    Add __init__.py

---
+++ benchmark/__init__.py
@@ -0,0 +1,6 @@
+"""Benchmarking module for AlgoBench."""
+
+from benchmark.benchmark import run_benchmark, run_single_benchmark
+from benchmark.complexity import format_complexity_panel
+
+__all__ = ["run_benchmark", "run_single_benchmark", "format_complexity_panel"]
