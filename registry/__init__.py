2fd12889fd4494cc9e6e7cdd06e86ce726078ad5
Yaswanth <yaswanthvanacharla@gmail.com>
2026-09-10 21:44:55 +0530

    Add __init__.py

---
+++ registry/__init__.py
@@ -0,0 +1,15 @@
+"""Algorithm registry for AlgoBench."""
+
+from registry.algorithm_registry import (
+    ALGORITHM_REGISTRY,
+    get_algorithm,
+    get_all_algorithms,
+    get_algorithm_names,
+)
+
+__all__ = [
+    "ALGORITHM_REGISTRY",
+    "get_algorithm",
+    "get_all_algorithms",
+    "get_algorithm_names",
+]
